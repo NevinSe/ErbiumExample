a56afe11b9023125943a20817f4b4103882211c8
unknown <kalawhar@gmail.com>
2020-11-12 08:13:13 -0600

    init

---
+++ ErbiumProblemSolvingProblems/ProblemSolvingProblems.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ErbiumProblemSolvingProblems
+{
+    class ProblemSolvingProblems
+    {
+       
+        //aaabbcccc
+        //3a2b4c
+        public void CompressString(string wordToCompress)
+        {
+            int counter = 1;
+            string compressedWord = "";
+            for(int i = 0; i < wordToCompress.Length; i++)
+            {
+                if(i != wordToCompress.Length - 1)
+                {
+                    if (wordToCompress[i] == wordToCompress[i + 1])
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        compressedWord += counter + wordToCompress[i].ToString();
+                        counter = 1;
+                    }
+                }
+                else
+                {
+                    compressedWord += counter + wordToCompress[i].ToString();
+                }
+               
+            }
+            Console.WriteLine(compressedWord);
+        }
+        public string ReverseString(string forwardWord)
+        {
+            string reversedStr = "";
+            for (int i = forwardWord.Length - 1; i >= 0; i--)
+            {
+                reversedStr += forwardWord[i].ToString();
+            }
+            return reversedStr;
+            //return string.Join("", forwardWord.ToCharArray().Reverse());
+        }
+        public bool Palindrome(string wordToCheck)
+        {
+            if (wordToCheck == ReverseString(wordToCheck))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void example()
+        {
+            Person person = new Person();
+        }
+    }
+}
---
+++ ErbiumProblemSolvingProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErbiumProblemSolvingProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemSolvingProblems problemSolvingProblems = new ProblemSolvingProblems();
+            //problemSolvingProblems.CompressString("aaabbcccc");
+            //string response = problemSolvingProblems.ReverseString("hello");
+            bool response = problemSolvingProblems.Palindrome("hello");
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
